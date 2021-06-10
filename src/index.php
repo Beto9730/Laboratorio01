ca697e4766f80874cfdbfed87ea089f20fd01452
Alberto <aleduurna@gmail.com>
2021-06-10 16:42:34 -0500

    se cambio la version 05 a version 6

--- src/index.php
+++ src/index.php
@@ -2,10 +2,10 @@
 for ($x = 0; $x <= 10; $x++) {
 
         if ($x <= 1) {
-                echo "$x GIT VERSION 05 ALBERTO  <br>";
+                echo "$x GIT VERSION 06 ALBERTO  <br>";
    
         }elseif ($x > 1) {
-                echo "$x GIT VERSION 05 ALBERTO  <br>";       
+                echo "$x GIT VERSION 06 ALBERTO  <br>";       
         } 
  
 }
