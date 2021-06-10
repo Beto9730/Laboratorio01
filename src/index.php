8f1e162ccbbcfe35dd6a7a1a78135ef43e7bd9f6
Alberto <aleduurna@gmail.com>
2021-06-10 17:34:10 -0500

    se cambio version 14  a version 15

--- src/index.php
+++ src/index.php
@@ -2,10 +2,10 @@
 for ($x = 0; $x <= 10; $x++) {
 
         if ($x <= 1) {
-                echo "$x GIT VERSION 14 ALBERTO  <br>";
+                echo "$x GIT VERSION 15 ALBERTO  <br>";
    
         }elseif ($x > 1) {
-                echo "$x GIT VERSION 14 ALBERTO  <br>";       
+                echo "$x GIT VERSION 15 ALBERTO  <br>";       
         } 
  
 }
