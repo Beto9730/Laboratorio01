b3a4326d43ab30be3fae4c8269ec901bbd17a529
Alberto <aleduurna@gmail.com>
2021-06-10 16:16:38 -0500

    Se cambio la version 03 a version04

--- src/index.php
+++ src/index.php
@@ -2,10 +2,10 @@
 for ($x = 0; $x <= 10; $x++) {
 
         if ($x <= 1) {
-                echo "$x GIT VERSION 03 ALBERTO  <br>";
+                echo "$x GIT VERSION 04 ALBERTO  <br>";
    
         }elseif ($x > 1) {
-                echo "$x GIT VERSION 03 ALBERTO  <br>";       
+                echo "$x GIT VERSION 04 ALBERTO  <br>";       
         } 
  
 }
